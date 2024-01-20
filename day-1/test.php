e9619d1a94fd54a4cc6a36573ba701b42513f3a0
Rahim Uddin <rahimuddin5915@gmail.com>
2024-01-20 16:20:38 +0600

    added something

--- day-1/test.php
+++ day-1/test.php
@@ -1,18 +1,21 @@
-<?php 
-$myName="Rahim Uddin";
-$age= 24;
+<?php
+$myName = "Rahim Uddin";
+$age = 24;
 print "{$myName} I am $age years old";
-$age=30;
+$age = 30;
 print "\n{$myName} I am $age years old";
 
 // php constant variable 
 /*multi
 line 
 comment */
-define("PI",3.1416);
-define("sscGpaRahim",4.36);
-echo "\nvalue of PI =".PI;
-$constant="constant";
-echo "\nvalue of PI={$constant('PI')}";
-echo "\nMy SSC result is={$constant('sscGpaRahim')}"
+define("PI", 3.1416);
+const name = "Rhaim Uddin";
 ?>
+<br></br>
+<?php echo name;
+define("sscGpaRahim", 4.36);
+echo "\nvalue of PI =" . PI;
+$constant = "constant";
+echo "\nvalue of PI={$constant('PI')}";
+echo "\nMy SSC result is={$constant('sscGpaRahim')}" ?>
